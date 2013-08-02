85977eee74c800b4855103f54bcf3111b69a8de3
Forthcome <contact@forthcome.fr>
2013-08-02 11:26:08 +0200

    Update and rename script.txt to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<!-- À placer en fin de fichier HTML / Put this piece of code at the end of your HTML file -->
+
+    <script type="text/javascript">
+(function(){for(var l=document.links,h=window.location.hostname,i=0,a;a=l[i];i++){if(a.hostname!=h){a.target='_blank';}}})();
+</script>
+
+<!-- Ce script permet de transformer tous les liens en liens sortants vers un nouvel onglet / all links open in a new tab
+
+Hand-picked by Frank Adebiaye using Google search and serendipity -->
